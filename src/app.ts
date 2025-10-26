3046eb8143d07d49875434eaa839c195d761a1a2
sifat26 <sifatict26@gmail.com>
2025-10-26 15:12:05 +0600

    image upload API implemented

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express, { Application, Request, Response } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import mainRouter from './app/routes';
 import httpStatus from 'http-status';
+import path from 'path';
 
 const app: Application = express();
 
@@ -21,6 +22,7 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(process.cwd(), 'public/uploads')));
 
 // Main API Routes
 app.use('/api/v1', mainRouter);
