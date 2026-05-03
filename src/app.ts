3ed54e5cf25632c3be01d6ecf16e33ef524dc794
Asad9340 <asadulimran1999@gmail.com>
2026-05-03 23:07:32 +0600

    updated app file

--- src/app.ts
+++ src/app.ts
@@ -14,10 +14,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
-      'http://localhost:3001',
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'https://outfitro.com',
+      'http://localhost:3001'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
